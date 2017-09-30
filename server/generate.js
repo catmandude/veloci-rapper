2dda00fe4ff78c884c7bfce4c976c1ae0652fb76
catmandude <ozziemiller555@gmail.com>
2017-09-29 22:38:21 -0500

    comments

--- server/generate.js
+++ server/generate.js
@@ -1,5 +1,16 @@
-
-
 export default (req, res, next) => {
-  console.log(next);
+  // create a lambda
+  // create a db
+  // configure lambda // configure DB
+  // create s3 bucket and write to it / read from it
+  // read from github
+  // tie all AWS
+  // Build SAM thing
+
+  // Get mammal code
+  // push SAM to Cloud Formation
+  // Call SDK for DB
+  // Push Mammal to S3
+  // Respond to Trike that all went well or not 
+  // DO WE NEED ANYTHING ELSE
 };
