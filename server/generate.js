86bf49b6ec09324dd775a134a1a576998babc802
Lauren Hilton <lauren.hilton@daugherty.com>
2017-09-30 17:16:52 -0500

    write to file

--- server/generate.js
+++ server/generate.js
@@ -1,4 +1,43 @@
+import fs from 'fs';
+
 export default (req, res, next) => {
+  const body = JSON.parse(Object.keys(request.body)[0]);
+  let output = [].concat({
+    purpose: 'Create regional lambdas',
+    call: '',
+    arguments: '',
+  })
+    .concat({
+      purpose: 'Create personal lambdas',
+      call: '',
+      arguments: '',
+    })
+    .concat({
+      purpose: 'Create person dbs',
+      call: '',
+      arguments: '',
+    })
+    .concat({
+      purpose: 'Create API gateway',
+      call: '',
+      arguments: '',
+    })
+    .concat({
+      purpose: 'Create VPC (Virtual Private Cloud)',
+      call: '',
+      arguments: '',
+    })
+    .concat({
+      purpose: 'Get code from git source',
+      call: '',
+      arguments: '',
+    });
+
+  fs.writeFile('mammal.json', JSON.stringify(output, null, 2), (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+    res.sendStatus(200);
+  });
   // create a lambda
   // create a db
   // configure lambda // configure DB
--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
+import Express from 'express';
 import './common/env';
-import Server from './common/server';
-import routes from './routes';
+import * as bodyParser from 'body-parser';
+import generate from './generate';
 
-export default new Server()
-  .router(routes)
-  .listen(process.env.PORT);
+const app = new Express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/api/v1/generate', generate);
+
+app.listen(process.env.PORT);
